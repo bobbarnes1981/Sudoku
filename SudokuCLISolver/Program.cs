6a59429b9a08911d28fc670da72ed3157af771e4
Robert Barnes <robertbarnes1981@googlemail.com>
2014-12-12 21:37:09 +0000

    Add project and solution files

---
+++ SudokuCLISolver/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SudokuLibrary;
+
+namespace SudokuCLISolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solver s = new Solver();
+            s.Solve();
+            Console.WriteLine(s.ToString());
+        }
+    }
+}
---
+++ SudokuLibrary/Solver.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuLibrary
+{
+    public class Solver
+    {
+        public void Solve()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
